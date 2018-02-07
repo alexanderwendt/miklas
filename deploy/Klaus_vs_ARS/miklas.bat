6863815b3ddb5a2d837b855f490e520f19b45104
wendt <alexander.wendt@tuwien.ac.at>
2018-02-07 18:19:56 +0100

    Removed deploys

--- deploy/Klaus_vs_ARS/miklas.bat
+++
@@ -1,7 +0,0 @@
-@Echo off
-set curdir=%CD%
-
-ECHO Starting Miklas 1.0 by Alexander Wendt 2014
-cmd mode con: cols=150 lines=200 /c start "Miklas 1.0 by Alexander Wendt 2014" /D  %curdir% /min java -jar bin\miklas.jar
-
-TIMEOUT 1
